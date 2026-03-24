1bc008cef51ccf374267beea50946e39518260c1
rlatlsdn98 <rlatlsdn98@naver.com>
2026-03-24 11:24:56 +0900

    init commit

---
+++ src/main/java/com/ohgiraffers/team3backendkms/Team3BackendKmsApplication.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.team3backendkms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team3BackendKmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Team3BackendKmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ohgiraffers/team3backendkms/Team3BackendKmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.team3backendkms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team3BackendKmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
