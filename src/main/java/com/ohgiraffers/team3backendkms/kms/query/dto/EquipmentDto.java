b5f527798e468411b403bbd1be9e8f7ee03a0abe
LimRaynor <morden9137@gmail.com>
2026-04-05 02:29:58 +0900

    [feat] 태그/설비 조회용 DTO 추가 (TagDto, EquipmentDto)

---
+++ src/main/java/com/ohgiraffers/team3backendkms/kms/query/dto/EquipmentDto.java
@@ -0,0 +1,14 @@
+package com.ohgiraffers.team3backendkms.kms.query.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class EquipmentDto {
+
+    private Long equipmentId;
+    private String equipmentName;
+}
