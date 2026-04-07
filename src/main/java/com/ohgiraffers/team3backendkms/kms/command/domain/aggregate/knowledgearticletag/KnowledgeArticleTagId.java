1aa4d7fd3ed11ccc201372eb246b72235a0c5f99
rlatlsdn98 <rlatlsdn98@naver.com>
2026-04-07 14:17:26 +0900

    tag table entity 추가

---
+++ src/main/java/com/ohgiraffers/team3backendkms/kms/command/domain/aggregate/knowledgearticletag/KnowledgeArticleTagId.java
@@ -0,0 +1,34 @@
+package com.ohgiraffers.team3backendkms.kms.command.domain.aggregate.knowledgearticletag;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.*;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Embeddable
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class KnowledgeArticleTagId implements Serializable {
+
+    @Column(name = "tag_id")
+    private Long tagId;
+
+    @Column(name = "article_id")
+    private Long articleId;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        KnowledgeArticleTagId that = (KnowledgeArticleTagId) o;
+        return Objects.equals(tagId, that.tagId) && Objects.equals(articleId, that.articleId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(tagId, articleId);
+    }
+}
---
+++ src/main/java/com/ohgiraffers/team3backendkms/kms/command/domain/aggregate/knowledgetag/KnowledgeTag.java
@@ -0,0 +1,43 @@
+package com.ohgiraffers.team3backendkms.kms.command.domain.aggregate.knowledgetag;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "knowledge_tag")
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+@AllArgsConstructor
+public class KnowledgeTag {
+
+    @Id
+    private Long tagId;
+
+    @Column(name = "tag_name", nullable = false, unique = true)
+    private String tagName;
+
+    @CreatedDate
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @CreatedBy
+    @Column(name = "created_by", updatable = false)
+    private Long createdBy;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @LastModifiedBy
+    @Column(name = "updated_by")
+    private Long updatedBy;
+}
